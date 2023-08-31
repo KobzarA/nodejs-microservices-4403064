187d25658f579e67f3cde533bdd02e5a9316991d
AnatoliiKobzar <119481160+KobzarA@users.noreply.github.com>
2023-08-31 18:22:03 +0300

    feat:add register, update, unregister ability for catalog service in register-service

--- workspace/microservices/catalog-service/bin/start.js
+++ workspace/microservices/catalog-service/bin/start.js
@@ -22,6 +22,7 @@ const connectToMongoose = require("../lib/mongooseConnection"); // Function to c
 // config.redis.client = connectToRedis(config.redis.options);
 
 const app = require("../app"); // Express application
+const { default: axios } = require("axios");
 
 // Create the HTTP server with the express app
 const server = http.createServer(app);
@@ -30,6 +31,58 @@ const server = http.createServer(app);
 server.on("listening", () => {
   const addr = server.address();
   const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+
+  const register = async () =>
+    axios
+      .put(
+        `http://127.0.0.1:3080/register/${config.serviceName}/${
+          config.serviceVersion
+        }/${server.address().port}`
+      )
+      .catch((e) => console.log(e));
+
+  const unregister = async () =>
+    axios
+      .delete(
+        `http://127.0.0.1:3080/register/${config.serviceName}/${
+          config.serviceVersion
+        }/${server.address().port}`
+      )
+      .catch((e) => console.log(e));
+
+  register();
+
+  const interval = setInterval(register, 10000);
+
+  const cleanup = async () => {
+    const clean = false;
+
+    if (!clean) {
+      clearInterval(interval);
+      await unregister();
+    }
+  };
+
+  process.on("uncaughtException", async () => {
+    await cleanup();
+    process.exit(0);
+  });
+
+  process.on("SIGTERM", async () => {
+    await cleanup();
+    process.exit(0);
+  });
+
+  process.on("SIGINT", async () => {
+    await cleanup();
+    process.exit(0);
+  });
+
+  process.on("unhandledRejection", async () => {
+    await cleanup();
+    process.exit(0);
+  });
+
   console.info(
     `${config.serviceName}:${config.serviceVersion} listening on ${bind}`
   );
