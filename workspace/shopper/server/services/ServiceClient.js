9eb63e8623478017cf15688f1cea30fd167b0e60
AnatoliiKobzar <119481160+KobzarA@users.noreply.github.com>
2023-09-01 08:27:58 +0300

    feat:created ServiceClient which find service and transforms req with proper url

---
+++ workspace/shopper/server/services/ServiceClient.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+const config = require("../config");
+
+class ServiceClient {
+  static async getService(servicename) {
+    try {
+      const response = await axios.get(
+        `${config.registry.url}/find/${servicename}/${config.registry.version}`
+      );
+      if (!response.data.ip) {
+        throw new Error(
+          `Couldn't find ${servicename}:${config.registry.version}`
+        );
+      }
+      return response.data;
+    } catch (error) {
+      const errorMessage =
+        (error.response && error.response.data && error.data.errorMessage) ||
+        error.message;
+
+      throw new Error(errorMessage);
+    }
+  }
+
+  static async callService(servicename, reqOptions) {
+    const { ip, port } = await this.getService(servicename);
+    // eslint-disable-next-line no-param-reassign
+    reqOptions.url = `http://${ip}:${port}${reqOptions.url}`;
+    try {
+      const response = await axios.get(reqOptions);
+      return response.data;
+    } catch (error) {
+      const errorMessage =
+        (error.response && error.response.data && error.data.errorMessage) ||
+        error.message;
+
+      throw new Error(errorMessage);
+    }
+  }
+}
+
+module.exports = ServiceClient;
